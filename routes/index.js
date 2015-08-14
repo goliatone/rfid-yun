046bb095554eda02c2364ba9730d5ea962d9ae15
goliatone <burgosemi@gmail.com>
2015-08-13 23:36:24 -0400

    ROUTES: Add default values to payload

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express', host: 'sensuino.local' });
+    res.redirect('/rfid');
+    // res.render('index', { title: 'Express', host: 'sensuino.local' });
 });
 
+router.post('/echo', function(req, res){
+    console.log(req.body);
+  res.send(200);
+});
 
 var data;
 router.post('/rfid', function(req, res){
     console.log(req.body);
 
-    var cardCode = req.body.cc;
-    var facilityCode = req.body.fc;
-    var binary = req.body.bn;
+    var cardCode = req.body.cc || 'not set';
+    var facilityCode = req.body.fc || 'not set';
+    var binary = req.body.bn || 'not set';
 
     var payload = {
         cardCode: cardCode,
@@ -26,16 +31,16 @@ router.post('/rfid', function(req, res){
 });
 
 router.get('/rfid', function(req, res){
-    var cardCode = req.query.cc;
-    var facilityCode = req.query.fc;
-    var binary = req.query.bn;
+    var cardCode = req.query.cc || 'not set';
+    var facilityCode = req.query.fc || 'not set';
+    var binary = req.query.bn || 'not set';
 
     var payload = {
         cardCode: cardCode,
         facilityCode: facilityCode,
         binary: binary
     };
-
+console.log('PAYLOAD', JSON.stringify(payload, null, 4));
     res.render('index', {
         title: 'RFID Reader',
         host:'sensuino.local',

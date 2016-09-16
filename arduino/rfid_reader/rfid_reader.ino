fd6c603e576b611318ef134e02956a42a73cecc7
goliatone <burgosemi@gmail.com>
2016-09-16 13:29:54 -0400

    Update template layout description

--- arduino/rfid_reader/rfid_reader.ino
+++ arduino/rfid_reader/rfid_reader.ino
@@ -6,8 +6,8 @@
  *
  * Wiegand HID 26 bit card reader.
  * Format:
- * Parity |<-Facility Code->| <---------------  Card Code  -------------->
- *    0 | 1 2 3 4 5 6 7 8 9 | 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+ * Parity|<-Facility Code->| <----------------  Card Code  ---------------> | Checksum
+ *     0 | 1 2 3 4 5 6 7 8 | 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 | 25
  *
  * More here: http://www.pagemac.com/azure/data_formats.php
  */
